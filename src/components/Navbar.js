56a087f38106ee1aeec5a1b8d0469ac4d00fe59e
Harshit671 <harshitgpt45@gmail.com>
2021-09-01 19:24:26 +0530

    navbar fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { useStateValue } from '../context/authcontext';
 import '../media/home.css'
 import Firebase from '../services/Firebase';
+import Data from '../data/navs-header-main.json'
 
 const Navbar = () => {
     const [{ user }, dispatch] = useStateValue();
@@ -18,18 +19,28 @@ const Navbar = () => {
 
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="nav navbar-nav navbar-center">
-                    <li className="nav-item active">
-                        <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
-                    </li>
-                    <li className="nav-item" >
-                        <Link to="/assetlist"><a className="nav-link" href="#">Assets</a></Link>
-                    </li>
-                    <li className="nav-item" >
-                        <Link to="/offerlist"><a className="nav-link" href="#">Offers</a></Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="/offerrequest"><a className="nav-link" href="#">Transfers</a></Link>
-                    </li>
+                    {
+                        Data.data.map((item, index) => {
+                            return (
+                                <li key={index} className={`nav-item ${item.actv}`}>
+                                    <Link to={item.link}><a className="nav-link" href="#">{item.name}</a></Link>
+                                </li>
+                            )
+                        })
+                        //                     <li className="nav-item active">
+                        //                         <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
+                        //                     </li>
+                        //                     <li className="nav-item" >
+                        //                         <Link to="/assetlist"><a className="nav-link" href="#">Assets</a></Link>
+                        //                     </li>
+                        //                     <li className="nav-item" >
+                        //                         <Link to="/offerlist"><a className="nav-link" href="#">Offers</a></Link>
+                        //                     </li>
+                        //                     <li className="nav-item">
+                        //                         <Link to="/offerrequest"><a className="nav-link" href="#">Transfers</a></Link>
+                        //                     </li>
+                        // }
+                    }
                 </ul>
                 <div className="navbar-nav ms-auto">
                     <button className="btn btn-outline-success my-2 my-sm-0 ml-3" onClick={handleLogout} >Logout</button>
